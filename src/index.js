f12c25cb728009c24ce6a730036c7ebecacb36c2
Ansel <anselcolvin@gmail.com>
2017-09-24 21:22:02 -0400

    Working on skeleton

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-// Create a new component. This component should produce some HTML.
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// Create a new component. This component should produce some HTML.
+const App = () => {
+   return <div>Hello world!</div>;
+}
 
 // Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
